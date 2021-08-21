ab8e0d995e7256b2a58323fd1eb9bc69d2a3fb18
yohancenturion <yohancent@gmail.com>
2021-08-21 13:23:17 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,31 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
+    
+    document.getElementById("submitBtn").addEventListener("click", function(e) {
+        
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPassword");
+        let Camposcompletos = true;
+
+        if(inputEmail.value==='') {
+            Camposcompletos = false;
+            inputEmail.classList.add("invalid");
+        } else {
+            inputEmail.classList.remove("invalid")
+        }
+
+        if(inputPassword.value===''){
+            inputPassword.classList.add("invalid");
+            Camposcompletos = false;
+        } else {
+            inputPassword.classList.remove("invalid");
+    }
+    if (Camposcompletos) {
+        window.location="inicio.html";
+    } else { alert ("Debe ingresar los datos"); }
+    })
+    
 
 });
